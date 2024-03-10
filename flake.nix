92c4a333034387202d5b3e98f6264eb77b0f9875
sagawao <heutronica@gmail.com>
2024-03-10 15:54:48 +0900

    delete comment out

--- flake.nix
+++ flake.nix
@@ -17,12 +17,6 @@
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-	 # home-manager.nixosModules.home-manager
-	  #{
-           # home-manager.useGlobalPkgs = true;
-	    #home-manager.useUserPackages = true;
-	    #home-manager.users.sagawao = import ./home/home.nix;
-	  #}
         ];
       };
     };
