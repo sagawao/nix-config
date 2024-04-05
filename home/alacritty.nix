3b64955aab9b2579724bf3fb59c7435987c57d9f
sagawao <heutronica@gmail.com>
2024-04-06 08:40:25 +0900

    fix padding

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -5,20 +5,12 @@
     enable = true;
     settings = {
       window = {
-        padding.x = 20;
+        padding.x = 10;
         padding.y = 10;
       };
       font.size = 7;
       font.offset.y = 5;
       import = [ "${pkgs.alacritty-theme}/solarized_light.toml" ];
-      shell = {
-        program = "${pkgs.zsh}/bin/zsh";
-        args = [ 
-          "-l"
-          "-c"
-          "zellij attach --index 0 --create"
-        ];
-      };
     };
   };
 }
