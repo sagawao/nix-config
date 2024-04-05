36f1c4d587d36f2d89d3a8b4d0baa4a3a8473f3c
sagawao <heutronica@gmail.com>
2024-04-06 08:56:09 +0900

    separate ddc.vim config to lua

--- home/nvim/ddc.nix
+++ home/nvim/ddc.nix
@@ -56,24 +56,6 @@ in
             })
         ];
 
-        extraConfigLua = ''
-            vim.fn["ddc#custom#patch_global"]('ui', 'native')
-            vim.fn["ddc#custom#patch_global"]('sources', {'around', 'lsp'})
-            vim.fn["ddc#custom#patch_global"]('sourceOptions', {
-                _ = {
-                    matchers = {'matcher_fuzzy'},
-                    sorters = {'sorter_fuzzy'},
-                    converters = {'converter_fuzzy'},
-                },
-                lsp = {
-                    mark = 'lsp',
-                    forceCompletionPattern = '\\.\\w*|:\\w*|->\\w*',
-                },
-            })
-            vim.fn["ddc#enable"]()
-            vim.fn["signature_help#enable"]()
-            vim.fn["popup_preview#enable"]()
-        '';
-
+        extraConfigLua = (builtins.readFile ./ddc.lua);
     };
 }
